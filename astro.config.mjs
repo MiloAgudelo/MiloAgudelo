c0f2fc083acccd2b5612b8ebe372d948b0d7a82b
Milo Agudelo <agudelojaramillocamilo00@Gmail.com>
2026-05-19 17:13:12 -0500

    feat(SEO): sitemap automático y robots.txt básico

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miloagudelo.com',
@@ -18,4 +20,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
