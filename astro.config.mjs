4d18367bc56c1ea37334577577721811007f5df8
Milo Agudelo <agudelojaramillocamilo00@Gmail.com>
2026-05-19 23:51:58 -0500

    fix: corregir redirects estáticos y fuente mono

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miloagudelo.com',
+  redirects: {
+    '/': '/coming-soon',
+    '/en': '/en/coming-soon',
+  },
 
   fonts: [
     {
